bd49317b21551c68e82157391cb30df6aeb71583
Thurston <tylerthurston1@gmail.com>
2017-06-15 13:08:08 -0400

    Added starting hidden, color reset, and list switch

--- JQuery/noob/js/script.js
+++ JQuery/noob/js/script.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
     $("li").click(function() {
         $(this).css("color", "#"+((1<<24)*Math.random()|0).toString(16));
     });
+    $("button").click(function() {
+        $("li").css("color", "black");
+    });
+    $("h1").click(function() {
+        if($("h2").is(":visible")) {
+            $("h2").hide();
+        }
+        else {
+            $("h2").show()
+        }
+    });
 });
