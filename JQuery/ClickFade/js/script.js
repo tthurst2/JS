72af0379700b4d366147eb53b22b4d8599df2cba
Thurston <tylerthurston1@gmail.com>
2017-06-20 11:40:31 -0400

    added neat profile picture addition, as well as this implementation

--- JQuery/ClickFade/js/script.js
+++ JQuery/ClickFade/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
    $("div").mouseenter(function() {
-      $("div").fadeTo("fast", 1); 
+      $(this).fadeTo("fast", 1); 
    });
    $("div").mouseleave(function() {
-      $("div").fadeTo("fast",0.5); 
+      $(this).fadeTo("fast",0.5); 
    });
    $("div").mousedown(function() {
-      $("div").css("background-color", "#5599FF");
+      $(this).css("background-color", "#5599FF");
    });
    $("div").mouseup(function() {
-      $("div").css("background-color", "#69D2E7");
+      $(this).css("background-color", "#69D2E7");
    });
 });
